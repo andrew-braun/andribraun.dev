8a7239b70191dc4f6db4fb80efd6bff79bcd77f1
andrew-braun <andrew@andrewbraun.dev>
2024-11-05 23:43:55 +0400

    feat: Rework hero section

--- src/components/sections/hero/MainHero.svelte
+++ src/components/sections/hero/MainHero.svelte
@@ -1,45 +1,25 @@
 <script>
+	import Title from '$components/text/Title.svelte';
 	import Button from '$components/ui/buttons/Button.svelte';
 </script>
 
 <section class="hero">
+	<Title superTitle="Hi! I'm" title="Andri Braun" tagAttributes={{ id: 'hero-title' }} />
 	<div class="text">
-		<p class="impact">I</p>
-		<p class="impact">write code,</p>
-		<p class="impact">build websites,</p>
+		<p class="impact">I&nbsp;</p>
+		<p class="impact">write code,&nbsp;</p>
+		<p class="impact">build websites,&nbsp;</p>
 		<p class="impact">and do other nerd things.</p>
-		<Button>Do Nerd Things for Me</Button>
 	</div>
-	<div class="other"></div>
+	<Button>Do Nerd Things for Me</Button>
 </section>
 
 <style lang="scss">
 	.hero {
-		display: grid;
-		grid-template-columns: 1fr 2fr;
-		grid-template-areas: 'text other';
-		padding: var(--gutters) var(--spacing-lg);
+		padding: var(--spacing-vertical-section) var(--spacing-gutters);
+	}
 
-		// @media (max-width: $breakpoint-sm) {
-		// 	grid-template-columns: 1fr;
-		// 	grid-template-areas: 'text' 'other';
-		// }
-
-		.text {
-			grid-area: text;
-
-			p {
-				margin: 0;
-			}
-
-			.impact {
-				font-size: var(--font-size-h1);
-				font-weight: 600;
-			}
-		}
-
-		.other {
-			grid-area: other;
-		}
+	.text {
+		display: flex;
 	}
 </style>
---
+++ src/components/text/Title.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	export let tag = 'h1';
+	export let tagAttributes = {};
+	export let superTitle = '';
+	export let title = '';
+	export let subtitle = '';
+</script>
+
+<svelte:element this={tag} class="title" {...tagAttributes}>
+	{#if superTitle}
+		<span class="title title-small">{superTitle}</span>
+	{/if}
+	<span class="title">{title}</span>
+	{#if subtitle}
+		<span class="title subtitle">{subtitle}</span>
+	{/if}
+</svelte:element>
+
+<style lang="scss">
+	.title {
+		display: flex;
+		flex-direction: column;
+		margin: 0;
+		font-size: var(--font-size-h1);
+		font-weight: 600;
+	}
+
+	.title-small {
+		font-size: var(--font-size-md);
+		font-weight: 400;
+	}
+
+	.subtitle {
+		font-size: var(--font-size-h3);
+		font-weight: 400;
+	}
+</style>
