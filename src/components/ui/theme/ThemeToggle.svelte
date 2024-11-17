86a3938a888b0790f4528ed9e28a8d1380410e56
andrew-braun <andrew@andrewbraun.dev>
2024-11-17 18:03:42 +0400

    feat: Add theme toggle switch

--- src/components/ui/theme/ThemeToggle.svelte
+++ src/components/ui/theme/ThemeToggle.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import Button from '$components/ui/buttons/Button.svelte';
-	let currentTheme: Theme = 'dark';
+	import { Switch } from "bits-ui";
+	import { onMount } from "svelte";
+	let currentTheme: Theme = "dark";
 
-	type Theme = 'dark' | 'light' | 'neon';
+	type Theme = "dark" | "light" | "neon";
 
-	const possibleThemes = ['dark', 'light'];
+	const possibleThemes = ["dark", "light"];
 
 	onMount(() => {
-		const storedTheme = localStorage.getItem('storedTheme');
+		const storedTheme = localStorage.getItem("storedTheme");
 		if (storedTheme) {
 			currentTheme = storedTheme as Theme;
 		}
@@ -20,9 +20,44 @@
 		const nextThemeIndex = (currentThemeIndex + 1) % possibleThemes.length;
 		currentTheme = possibleThemes[nextThemeIndex] as Theme;
 
-		document.body.setAttribute('data-theme', currentTheme);
-		localStorage.setItem('storedTheme', currentTheme);
+		document.body.setAttribute("data-theme", currentTheme);
+		localStorage.setItem("storedTheme", currentTheme);
 	};
 </script>
 
-<Button on:click={toggleTheme}>Switch Theme</Button>
+<div class="theme-switch-wrapper">
+	<Switch.Root checked={currentTheme === "dark"} on:click={toggleTheme} class="theme-switch">
+		<Switch.Thumb class="theme-switch-thumb" />
+	</Switch.Root>
+</div>
+
+<style lang="scss">
+	.theme-switch-wrapper {
+		:global {
+			.theme-switch {
+				position: relative;
+				width: 60px;
+				aspect-ratio: 5/ 3;
+				border: none;
+				border-radius: var(--border-radius-xl);
+				background: var(--color-primary);
+				transition: var(--transition-md);
+
+				&[data-state="checked"] {
+					.theme-switch-thumb {
+						transform: translateX(100%);
+					}
+				}
+
+				.theme-switch-thumb {
+					display: flex;
+					width: 50%;
+					aspect-ratio: 1;
+					border-radius: 50%;
+					background: var(--color-background);
+					transition: var(--transition-md);
+				}
+			}
+		}
+	}
+</style>
